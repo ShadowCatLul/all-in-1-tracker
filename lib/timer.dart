53c10e2a1090ff3701d8899bc1b943e42ab2dae8
ShadowCatLul <xivx@mail.ru>
2024-08-26 00:48:21 +0300

    feature/timer: стартовые попытки в таймер

---
+++ lib/timer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+
+
+class Timer extends StatelessWidget {
+  const Timer({
+    super.key,
+  });
+
+ @override
+  Widget build(BuildContext context) {
+
+return CircularCountDownTimer(
+     duration: 10,
+     initialDuration: 0,
+     controller: CountDownController(),
+     width: MediaQuery.of(context).size.width / 2,
+     height: MediaQuery.of(context).size.height / 2,
+     ringColor: Colors.grey[300]!,
+     ringGradient: null,
+     fillColor: Colors.purpleAccent[100]!,
+     fillGradient: null,
+     backgroundColor: Colors.purple[500],
+     backgroundGradient: null,
+     strokeWidth: 20.0,
+     strokeCap: StrokeCap.round,
+     textStyle: TextStyle(
+         fontSize: 33.0, color: Colors.white, fontWeight: FontWeight.bold),
+     textAlign: TextAlign.center,
+     textFormat: CountdownTextFormat.S,
+     isReverse: false,
+     isReverseAnimation: false,
+     isTimerTextShown: true,
+     autoStart: false,
+     onStart: () {
+        debugPrint('Countdown Started');
+     },
+     onComplete: () {
+        debugPrint('Countdown Ended');
+     },
+     onChange: (String timeStamp) {
+        debugPrint('Countdown Changed $timeStamp');
+     },
+     timeFormatterFunction: (defaultFormatterFunction, duration) {
+        if (duration.inSeconds == 0) {
+           return "Start";
+        } else {
+           return Function.apply(defaultFormatterFunction, [duration]);
+        }
+     },
+ );
+
+  }
+}
